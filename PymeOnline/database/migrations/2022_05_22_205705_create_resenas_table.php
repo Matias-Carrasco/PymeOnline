70a377f447002a9f977f19f54da00008e50c0156
sebastian <sebastian.riquelme1801@alumnos.ubiobio.cl>
2022-05-22 18:29:59 -0400

    fix resena

--- PymeOnline/database/migrations/2022_05_22_205705_create_resenas_table.php
+++ PymeOnline/database/migrations/2022_05_22_205705_create_resenas_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use PhpParser\Node\Expr\Cast\String_;
+
 
 class CreateResenasTable extends Migration
 {
