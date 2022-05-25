7bb3bd391a3f0515dbbf48e2ff4166aadd358596
BasCandia <bcandia.0123@gmail.com>
2022-05-25 19:31:59 -0400

    Index para usuarios, Vermas para Cliente y Tienda

---
+++ PymeOnline/app/Http/Controllers/AdminController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\rol;
+use App\Models\cliente;
+use App\Models\tienda;
+use App\Models\direccion;
+use App\Models\comuna;
+
+class AdminController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $rol = rol::all();
+        $usuarios = User::all();
+        
+        return view('administra.index',compact('usuarios'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        $usuariosVermas = User::where('usuario_id',$id)->value('rol_id');
+        if($usuariosVermas == 2){
+            $datosVermas = cliente::where('usuario_id',$id)->first();
+            return view('administra.show', compact('datosVermas'));
+        }elseif ($usuariosVermas == 3){
+            $datosVermas = tienda::where('usuario_id',$id)->first();
+            $idDireccion = tienda::where('usuario_id',$id)->value('direccion_id');
+            $direccionTienda = direccion::where('direccion_id',$idDireccion)->first();
+            $idComuna = direccion::where('direccion_id',$idDireccion)->value('comuna_id');
+            $comunaTienda = comuna::where('comuna_id',$idComuna)->first();
+            return view('administra.showTienda', compact('datosVermas','comunaTienda','direccionTienda'));
+        }        
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
