53cd4c1655d271219809f4517f63589284003abe
BasCandia <bcandia.0123@gmail.com>
2022-05-25 16:27:20 -0400

    modificacion a tabla users

--- PymeOnline/database/migrations/2014_10_12_000001_create_users_table.php
+++ PymeOnline/database/migrations/2014_10_12_000001_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('Baneado')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
