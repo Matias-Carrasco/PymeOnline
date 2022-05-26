3c28c348eb0c5b8a2d053cfb9268a2ad9e54ea0f
DreamyBit <felipe.ims@outlook.es>
2022-05-25 20:54:03 -0400

    Inicio Seeders y coreccion de jerarquia

--- PymeOnline/database/migrations/2014_10_12_000001_create_users_table.php
+++ PymeOnline/database/migrations/2014_10_12_000001_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->boolean('Baneado')->default(false);
+            $table->boolean('baneado')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
