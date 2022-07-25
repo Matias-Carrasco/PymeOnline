f3c7e23d53a230aff4e3be378720d1f3973b2aff
Matias-Carrasco <matfcvega@gmail.com>
2022-07-24 20:57:02 -0400

    unit test login

--- PymeOnline/database/factories/UserFactory.php
+++ PymeOnline/database/factories/UserFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
+use App\Models\tienda;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
@@ -15,8 +17,8 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
+            'rol_id' => random_int(2,3),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
@@ -34,6 +36,15 @@ class UserFactory extends Factory
             return [
                 'email_verified_at' => null,
             ];
+        });
+    }
+
+    public function configure()
+    {
+        return $this->afterCreating(function ( User $user ){
+            $tienda = tienda::find(1);
+            $tienda->id = $user->id;
+            $tienda->save();
         });
     }
 }
