4f0bff26224fb9bc9a4d096ebd141132fb6f08b6
Felipe Muñoz Sanchez <felipe.ims@outlook.es>
2022-08-17 22:31:08 -0400

    visualizacion de reseñas

--- PymeOnline/app/Http/Controllers/ResenaController.php
+++ PymeOnline/app/Http/Controllers/ResenaController.php
@@ -7,79 +7,21 @@ use Illuminate\Http\Request;
 
 class ResenaController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+
+    public function getList($id)
     {
-        //
+        $resenas = resena::where('publicacion_id','=',$id)->get();
+        return json_encode(array('data'=>$resenas));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function getScore($id)
     {
-        //
+        $resenas = resena::where('publicacion_id','=',$id)->get();
+        $promedio = 0;
+        foreach ($resenas as $re) {
+            $promedio += $re->resena_califacion;
+        }
+        return json_encode(array('data'=>$promedio / count($resenas)));
     }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\resena  $resena
-     * @return \Illuminate\Http\Response
-     */
-    public function show(resena $resena)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\resena  $resena
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(resena $resena)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\resena  $resena
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, resena $resena)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\resena  $resena
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(resena $resena)
-    {
-        //
-    }
+    
 }
--- PymeOnline/routes/web.php
+++ PymeOnline/routes/web.php
@@ -51,7 +51,8 @@ Route::middleware(['CheckBan'])->group(function () {
         Route::resource('producto', '\App\Http\Controllers\ProductoController');
         Route::resource( 'tags', TagController::class );
         Route::resource('publicacion', '\App\Http\Controllers\PublicacionController');
-
+        Route::get('publicacion/res/{id}','\App\Http\Controllers\ResenaController@getList')->name('PubliGetRes');
+        Route::get('publicacion/score/{id}','\App\Http\Controllers\ResenaController@getScore')->name('PubliGetScore');
     });
 
 });
